812eb1855b40cffb9dcbe65ba041ba68708520a2
Chris Matthieu <chris@matthieu.us>
2018-05-26 09:41:20 -0700

    it works

--- index.js
+++ index.js
@@ -3,20 +3,32 @@
 // });
 
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
 var IPFS = require('ipfs-api');
 var ipfsapi = IPFS({host: '127.0.0.1', port: '5001', protocol: 'http'})
 
-var Readable = require('stream').Readable;
-
 app.use(express.static('public'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('*', function (req, res) {
+app.post('/dag', function (req, res) {
+  var task = JSON.parse(req.body.task)
+  console.log("task", task);
+  ipfsapi.dag.put(task, { format: 'dag-cbor', hashAlg: 'sha3-512' }, (err, cid) => {
+    console.log(cid.toBaseEncodedString())
+    var dag = cid.toBaseEncodedString()
+
+    res.send({dag:dag});
+  })
+})
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 })
 
